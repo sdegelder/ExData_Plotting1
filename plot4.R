933142ffe0461401a34445f8f141c4644d375029
sdegelder <toto@toto.com>
2015-08-05 16:49:08 +0200

    pushing 4 PNGs and 4 R scripts

---
+++ plot4.R
@@ -0,0 +1,57 @@
+#This script should be placed in the same folder as the household_power_consumption.txt file
+#and this folder should be set as your work directory
+
+#reading a first subset of the household_power_consumption.txt file
+#by starting at the first row with Feb 1, 2007 row
+require(data.table)
+first.subset <- fread("household_power_consumption.txt",sep=";",header=FALSE,na.strings=c("?"),verbose=FALSE,skip="1/2/2007",colClasses=c("character","character","character","character","character","character","character","character","character"))
+
+#subseting our dataset to only keep data for Feb 1 and 2, 2007
+data.set <- subset(first.subset, (V1 == "1/2/2007")|(V1 == "2/2/2007"))
+
+#adding clean variable names
+setnames(data.set,c("V1","V2","V3","V4","V5","V6","V7","V8","V9"),c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#merging Date and Time columns, and converting dates from char to date format
+new.date <- paste(as.Date(data.set$Date,"%d/%m/%Y"),data.set$Time,sep=" ")
+new.date <- as.character(new.date)
+#converting date and time to POSIXct format
+new.date <- as.POSIXct(new.date, tz = "")
+
+#merging our new date column with our dataset
+data.set <- cbind(new.date,data.set)
+#removing previous date and time column
+data.set <- subset(data.set, select=-c(Date,Time))
+setnames(data.set,"new.date","Date")
+
+##Generating the plot4 charts and png file
+data.set$Global_active_power <- as.numeric(data.set$Global_active_power)
+data.set$Global_reactive_power <- as.numeric(data.set$Global_reactive_power)
+data.set$Sub_metering_1 <- as.numeric(data.set$Sub_metering_1)
+data.set$Sub_metering_2 <- as.numeric(data.set$Sub_metering_2)
+data.set$Sub_metering_3 <- as.numeric(data.set$Sub_metering_3)
+data.set$Voltage <- as.numeric(data.set$Voltage)
+
+#Open png device
+png(file="plot4.png")
+
+#setting mfrow parameter to generate 2 rows of 2 plots
+par(mfrow = c(2,2))
+
+with(data.set,{
+  #top left plot
+  plot(Date, Global_active_power, ylab="Global Active Power",xlab="", type = "l")
+  #top right plot
+  plot(Date, Voltage, ylab="Voltage",xlab="datetime", type = "l")
+  #bottom left plot
+  plot(data.set$Date,data.set$Sub_metering_1, ylab="Energy sub metering",xlab="",type = "l")
+  points(data.set$Date,data.set$Sub_metering_1, col="black",type = "l")
+  points(data.set$Date,data.set$Sub_metering_2, col="red",type = "l")
+  points(data.set$Date,data.set$Sub_metering_3, col="blue",type = "l")
+  legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=c(1,1,1),bty = "n")
+  #bottom right plot
+  plot(Date, Global_reactive_power, ylab="Global_reactive_power",xlab="datetime", type = "l")
+})
+
+#Close the png file device
+dev.off()
